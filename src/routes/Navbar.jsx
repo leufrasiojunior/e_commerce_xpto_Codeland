861e4cfce520f4e61e1ce24cb80083f9f05531ef
leufrasiojunior <50365104+leufrasiojunior@users.noreply.github.com>
2023-07-05 11:43:12 -0300

    Update Navbar.jsx

--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Navbar() {
     return (
         <nav>
-            <Link to='/'>Home</Link>
+            <Link to='/'>Home Page Novo</Link>
             <Link to='/produtos'>Produtos</Link>
             <Link to='/detail'>Detalhes do Produto</Link>
         </nav>
