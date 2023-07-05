c2b53ed994e83c0bca5da5d2b295a9baee9a9ac0
leufrasiojunior@gmail.com <leufrasiojunior@gmail.com>
2023-07-05 19:26:00 -0300

    ajuste da tela de detalhes

--- src/routes/DetailsProduct.jsx
+++ src/routes/DetailsProduct.jsx
@@ -5,25 +5,39 @@ import { React, useState, useEffect } from "react";
 function DetailsProduct() {
     let { id } = useParams();
     // console.log(id);
-    const [produto, setProduto] = useState();
+    const [produto, setProduto] = useState([]);
     const [prodErro, setProdErro] = useState('');
+
     useEffect(() => {
         obterproduto();
     }, []);
 
     const obterproduto = () => {
         getIds(id)
-            .then((res) => {
-                setProduto(res)
-                console.log(res)
+            .then((resp) => {
+                setProduto(resp)
+                // console.log(resp)
             }
             )
             .catch((err) => setProdErro(err));
 
     };
     return (
-        <div></div>
-
+        <div>
+            {prodErro ? (
+                <div>
+                    <h2>Algo deu errado.</h2>
+                </div>
+            ) : (
+                <div>
+                    <div>
+                        {produto.id}
+                    </div>
+                    <div>{produto.nome}</div>
+                    <div>{produto.descricao}</div>
+                </div>
+            )}
+        </div>
     )
 }
 
