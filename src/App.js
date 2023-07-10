c95fe096801b6f47f8ad9d3795f7c282083003dd
leufrasiojunior@gmail.com <leufrasiojunior@gmail.com>
2023-07-09 23:29:55 -0300

    Home alterado e projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./routes/Navbar";
-import Lista from "./routes/Lista";
+// import Lista from "./routes/Lista";
 import Footer from "./routes/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Lista />
+      {/* <Lista /> */}
       <Outlet />
       <Footer />
     </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
 
-const Home = () => {
+import { React, useState, useEffect } from "react";
+import { getIds } from "../services/getIds";
+import { useNavigate } from "react-router-dom";
 
+function Home() {
+    const [ids, setIds] = useState([]);
+    const [idserro, setIdserro] = useState("");
+    const navigate = useNavigate();
+    function handleClick(item) {
+        // console.log(id);
+        navigate("/detail/" + item);
+    }
+    useEffect(() => {
+        reloadIds();
+    }, []);
+    const reloadIds = () => {
+        getIds()
+            .then((res) => setIds(res))
+            .catch((err) => setIdserro(err));
+    };
     return (
-        <div>
-
-        </div>
-    )
+        <div className="container" key="index">
+            {
+                idserro ? (
+                    <div>
+                        <h2>Algo deu errado.</h2>
+                    </div>
+                ) : (
+                    ids.map((i, index) => (
+                        <div>
+                            <div key={i.id} className="box">
+                                <div className="img"><img src={i.imagens[0].url} alt={i.nome} key={i.nome}></img></div>
+                                <div className="nome" key={i.nome}>{i.nome}</div>
+                                <div className="preco" key={i.preco}>R$: {i.preco}</div>
+                                <button className="button-17" onClick={() => handleClick(i.id)} key={index}>Ver detalhes</button>
+                            </div>
+                        </div>
+                    ))
+                )
+            }
+        </div >
+    );
 }
 
-export default Home
+export default Home;
